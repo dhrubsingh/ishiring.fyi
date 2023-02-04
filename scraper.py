db739d5e0753a41a3d7f0a7ebb736b4478200c31
Dhrubhagat <dhrubhagatjhoond@gmail.com>
2023-02-03 21:41:17 -0500

    modified to update database with scraped results

--- scraper.py
+++ scraper.py
@@ -16,7 +16,7 @@ def scrape_page(url):
     # get link from company name, if that doesn't exist get link from first note
     # if that doesn't exist, get it from the location
     links = {}
-    data = []
+    data = {}
 
     for i in range(0, len(td_elements), 3):
         # filter out for any jobs that are still closed
@@ -43,23 +43,62 @@ def scrape_page(url):
 
             # check if location has link
             # check if notes has link
+            data[name.get_text()] = {
+                "link": links[name.get_text()],
+                "location": location.get_text(),
+                "notes": notes.get_text()
+            }
             
+            """
             data.append({
                 "company": name.get_text(),
                 "link": links[name.get_text()],
                 "location": location.get_text(),
                 "notes": notes.get_text()
             })
+            """
 
     # instert/update database with information 
+
+    """# approach 1: delete database, repopulate with newly scraped links
+    cur.execute("DELETE from internships")
+    db.commit()
+
     for i in range(len(data)):
         cur.execute("INSERT INTO internships (id, company, link, location, notes) VALUES (?, ?, ?, ?, ?)", (i, data[i]["company"], data[i]["link"], data[i]["location"], data[i]["notes"]))
         db.commit()
+    """
 
+    # approach 2: modifying database
+    
+    comps = cur.execute("SELECT company from internships").fetchall()
+    companies = [str(row[0]) for row in comps]
+    
+    # delete any things in the table that isn't in the scraped data
+    for company in companies:
+        if company not in data:
+            cur.execute("DELETE FROM internships WHERE company = ?", (company,))
+            db.commit()
+    
+    # add any thing that is in the data table but not in the SQL table
+    for i, entry in enumerate(data.keys()):
+        if entry not in companies:
+             #print(i, entry, data[entry]["link"], data[entry]["location"], data[entry]["notes"])
+             cur.execute("INSERT INTO internships (id, company, link, location, notes) VALUES (?, ?, ?, ?, ?)", (i, entry, data[entry]["link"], data[entry]["location"], data[entry]["notes"]))
+             db.commit()
 
+    #print(data)
+
+    return data
+
+    """
     jsonify = json.dumps(data)
     return jsonify
+    """
 
+internships = scrape_page(urls[0])
+
+"""
 # convert list of dictionaries into a JSON file
 internships = scrape_page(urls[0])
 
@@ -69,6 +108,6 @@ with open("internships.json", "w") as outfile:
 new_grad = scrape_page(urls[1])
 with open("newgrad.json", "w") as outfile:
     outfile.write(new_grad)
-
+"""
        
 
