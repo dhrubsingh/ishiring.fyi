0646c7a90381e540e64e89ca8a006d7d4834bad2
Nikhil Datar <ndatar18@gmail.com>
2023-02-06 22:24:03 -0500

    start email post request

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from emails import companies, links, locations, notes
+import re
+import sqlite3
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
@@ -8,10 +12,27 @@ def index():
         links=links, locations=locations, notes=notes, zip=zip               
 )
     
-# @app.route('/signup')
-# def signup():
-#     pass
+@app.route('/signup', methods=["GET", "POST"])
+def signup():
+    if (request.method == "POST"):
+        user_email = request.form.get('#signup')
+        if (check(user_email)):
+            # Finish setting up alert in form.html
+            db = sqlite3.connect('./emails.db')
+            cursor = db.cursor()
+        else:
+            render_template('form.html', alert=True)
+    else:
+        return render_template('form.html')
+    
+
+def check(user):
+    email_format = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b'
+    if(re.fullmatch(email_format, user)):
+        return True
+    return False
 
 
 if __name__ == "__main__":
     app.run(debug=True)
+
